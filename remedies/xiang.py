7ac5c7dfdcd4fc4ee911d50cd4dd66956bc1c94e
GZ82 <44627267+GZ82@users.noreply.github.com>
2022-07-17 09:47:15 +0800

    Create xiang.py

---
+++ remedies/xiang.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""features to display
+Created on Thu 17 Jul 2022
+@author: Zhen
+"""
+
+####################################
+# show image of bhaisajyaguru
+####################################
