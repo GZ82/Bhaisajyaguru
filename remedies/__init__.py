5be10dbd1d22c2cd906ec4443f3e837f1e13d7e7
Zhen Guo <zhen.guo82@hotmail.com>
2022-08-14 13:53:40 +0800

    created a blog website

--- remedies/__init__.py
+++ remedies/__init__.py
@@ -67,6 +67,11 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    # register blueprint for blog
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
     return app
 
-    
+
