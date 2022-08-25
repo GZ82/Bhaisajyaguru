ee1112fb23c1293f7985c2adf1909f5c7948b3cb
aimeez <aimeeatsg@gmail.com>
2022-08-25 16:38:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from distutils.core import setup
 setup(name='remedies',
       version='1.0.0',
       py_modules=['remedies'],
+      install_requires=[
+        'flask',
+        'Flask-RESTful',
+      ],
+      author="ybbhfdf",
       )
+
 
 # from setuptools import find_packages, setup
 
