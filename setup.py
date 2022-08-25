9cc39ba4c646d6f4678ec19621f731ad8fce8cff
aimeez <aimeeatsg@gmail.com>
2022-08-25 16:44:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
-from distutils.core import setup
+# from distutils.core import setup
 
-setup(name='remedies',
-      version='1.0.0',
-      py_modules=['remedies'],
-      install_requires=[
-        'flask',
-        'Flask-RESTful',
-      ],
-      author="ybbhfdf",
-      )
-
-
-# from setuptools import find_packages, setup
-
-# if __name__ == "__main__":
-#       setup(
-#           name='remedies',
-#           version='1.0.0',
-#           packages=find_packages(),
-#           include_package_data=True,
-#           zip_safe=False,
-#           install_requires=[
-#               'flask',
-#               'Flask-RESTful',
-#               'poetry'
-#           ],
+# setup(name='remedies',
+#       version='1.0.0',
+#       py_modules=['remedies'],
+#       install_requires=[
+#         'flask',
+#         'Flask-RESTful',
+#       ],
+#       author="ybbhfdf",
 #       )
+
+
+from setuptools import find_packages, setup
+
+if __name__ == "__main__":
+      setup(
+          name='remedies',
+          version='1.0.0',
+          packages=find_packages(),
+          include_package_data=True,
+          zip_safe=False,
+          install_requires=[
+              'flask',
+              'Flask-RESTful',
+              'poetry'
+          ],
+      )
